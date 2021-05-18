ea64262dd7cb2eda585349cdd3751907dadef266
k0ngConvo <theepakorn@convolab.ai>
2021-05-18 15:27:13 +0700

    appium

--- appium_server.sh
+++ appium_server.sh
@@ -1,2 +1,3 @@
-appiumPort=$1
-appium -p $appiumPort --session-over --allow-insecure chromedriver_autodownload > /dev/null 2>&1
+appium
+# --session-over --allow-insecure chromedriver_autodownload
+#> /dev/null 2>&1
